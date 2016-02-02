994f89fd71593e902ff368f6238605e877f0f317
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-02-02 15:57:06 +0100

    commit

--- DynamicLinking/mpi_script.py
+++ DynamicLinking/mpi_script.py
@@ -1,5 +1,10 @@
 from mpi4py import MPI
+from ctypes import *
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-print("\thello world from process %d" % rank)
+print("\tin script: hello world from process %d" % rank)
+mystring = create_string_buffer(b"hello world from process %d" % rank)
+
+dso = CDLL("./hello.so")
+dso.char_hello(mystring)
